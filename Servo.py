255446ddd8176a09bdcab1722c08586fc079c603
George McCannon <145486946+gmccannon@users.noreply.github.com>
2024-11-20 20:08:49 -0500

    Update Servo.py to test back and fourth movement

--- Servo.py
+++ Servo.py
@@ -32,10 +32,20 @@ if __name__ == '__main__':
     print("If they have already been at 90°, nothing will happen.")
     print("Keep the program running!!! installing servos...")
     pwm=Servo()
-    while True:
-        try :
-            pwm.setServoPwm('0',90)
-            pwm.setServoPwm('1',90)
+    try:
+        while True:
+            for i in range(50,110,1):
+                pwm.setServoPwm('0',i)
+                time.sleep(0.01)
+            for i in range(110,50,-1):
+                pwm.setServoPwm('0',i)
+                time.sleep(0.01)
+            for i in range(80,150,1):
+                pwm.setServoPwm('1',i)
+                time.sleep(0.01)
+            for i in range(150,80,-1):
+                pwm.setServoPwm('1',i)
+                time.sleep(0.01)
         except KeyboardInterrupt:
-            print ("\nEnd of program")
-            break
+                pwm.setServoPwm('0',90)
+                pwm.setServoPwm('1',90)
